62aa59cd8507770a37dced6abcc7bd72940d8b9b
Jh0n16 <57267877+Jh0n16@users.noreply.github.com>
2024-03-13 16:06:14 -0300

    Mudando fuso-horário da applicação

--- resources/views/cliente/show.blade.php
+++ resources/views/cliente/show.blade.php
@@ -4,11 +4,15 @@
 @endsection
 
 @section('conteudo-principal')
+    <nav>
+        <a href="{{ route("cliente.index") }}">Visualizar clientes</a>
+    </nav>
+
     <p>Id: {{ $cliente->id }}</p>
     <p>Nome: {{ $cliente->nome }}</p>
     <p>Sobrenome: {{ $cliente->sobrenome }}</p>
     <p>Data de Nascimento: {{ \Carbon\Carbon::parse($cliente->dataDeNascimento)->tz("America/Sao_Paulo")->format("d/m/Y") }}</p>
-    <p>Criado em: {{ \Carbon\Carbon::parse($cliente->created_at)->tz("America/Sao_Paulo")->format("d/m/Y") }}</p>
-    <p>Editado em: {{ \Carbon\Carbon::parse($cliente->updated_at)->tz("America/Sao_Paulo")->format("d/m/Y") }}</p>
+    <p>Criado em: {{ \Carbon\Carbon::parse($cliente->created_at)->tz("America/Sao_Paulo")->format("d/m/Y - H:i:s") }}</p>
+    <p>Editado em: {{ \Carbon\Carbon::parse($cliente->updated_at)->tz("America/Sao_Paulo")->format("d/m/Y - H:i:s") }}</p>
     
 @endsection
